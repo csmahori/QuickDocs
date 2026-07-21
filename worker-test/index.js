8c12aff4f4983ecc990a243888b997621c2c8ba1
csmahori <csmahori@gmail.com>
2026-07-21 19:02:44 +0530

    Rename coupon code HARDA75 to HARDAPK75 (worker-test)

--- worker-test/index.js
+++ worker-test/index.js
@@ -24,7 +24,7 @@ const CORS = {
  */
 const COUPONS = {
   'SAVE50':  { pct: 50,  active: true },
-  'HARDA75': { pct: 75,  active: true },
+  'HARDAPK75': { pct: 75,  active: true },
   'PAHADI100': { pct: 100, active: true },
 };
 
